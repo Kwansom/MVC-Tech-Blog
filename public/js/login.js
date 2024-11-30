7153d82657d6c47cde8b3415fa5e1aaa326b2884
Kun Wansom <wansom.kun@gmail.com>
2024-11-29 18:42:33 -0800

    cleaned up login inputs

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,15 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-
-  const email = document.querySelector("#email-login").value.trim();
+  // const email = document.querySelector("#email-login").value.trim();
   const username = document.querySelector("#username-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
-  if (email && username && password) {
+  if (username && password) {
     // Send a POST request to the API endpoint
     const response = await fetch("/api/user/login", {
       method: "POST",
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     });
 
